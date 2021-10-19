ab56e5b5f7582e9ded2dc7a913da340eb234c888
Stas Goshovsky <stasgosha@yandex.ru>
2021-10-19 14:26:51 +0300

    - initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.workers__cards--slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    rtl:true,
+    arrows:true,
+    prevArrow:'<button type="button" class="slick-prev"><img src="images/arrow-slider-prev.svg" alt="arrow previous"></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-slider-next.svg" alt="arrow next"></button>',
+  });
