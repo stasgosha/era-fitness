5e20832fc2cd20ce9f9c9df48aa61cf2ac752779
Denis <oparenyuk02@gmail.com>
2021-10-22 11:00:59 +0300

    Era fitnnes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ $('.workers__cards--slider').slick({
     prevArrow:'<button type="button" class="slick-prev"><img src="images/arrow-slider-prev.svg" alt="arrow previous"></button>',
     nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-slider-next.svg" alt="arrow next"></button>',
   });
+
+
+
+
+
+
+  const header = document.querySelector('.header'),
+        headerWrapper = document.querySelector('.header__wrapper');
+
+
+
+  window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 20){
+        header.classList.add('active');
+        headerWrapper.classList.add('active');
+    }else{
+        header.classList.remove('active');
+        headerWrapper.classList.remove('active');
+    }
+  });
